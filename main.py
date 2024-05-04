b33057d67e8fc0ad647e49e0610cc973d56fa32b
Assami <minimiddo14@gmail.com>
2024-05-04 22:43:29 +0500

    fixed full

--- main.py
+++ main.py
@@ -12,6 +12,11 @@ from scipy import ndimage as ndi
 seeds = []
 
 
+def generate_fixed_colors(n_colors):
+    np.random.seed(42)
+    return [list(np.random.choice(range(256), size=3)) for _ in range(n_colors)]
+
+
 def load_image():
     global img, img_tk, path
     path = filedialog.askopenfilename()
@@ -22,84 +27,73 @@ def load_image():
 
 
 def apply_segmentation():
-    global img_tk, seeds
+    global img_tk, initial_seed, initial_region
     image_cv = cv2.imread(path)
+    image_cv_rgb = cv2.cvtColor(image_cv, cv2.COLOR_BGR2RGB)
     image_cv_lab = cv2.cvtColor(image_cv, cv2.COLOR_BGR2Lab)
-    if color_space_var.get() == "RGB":
-        image_cv = cv2.cvtColor(image_cv, cv2.COLOR_BGR2RGB)
 
     if segment_type.get() == 1:
         color = colorchooser.askcolor(title="Choose color")[0]
         if color:
             color = [int(c) for c in color]
-            image_cv = select_color(image_cv, color)
+            image_cv_rgb = select_color(image_cv_rgb, color)
             image_cv_lab = select_color(image_cv_lab, color)
 
-    # Flatten the image arrays
-    pixels = image_cv.reshape((-1, 3))
+    pixels_rgb = image_cv_rgb.reshape((-1, 3))
     pixels_lab = image_cv_lab.reshape((-1, 3))
 
     if segmentation_method.get() == "KMeans":
         num_clusters = int(entry_clusters.get())
-        clustering = KMeans(n_clusters=num_clusters)
-        labels = clustering.fit_predict(pixels)
+        clustering_rgb = KMeans(n_clusters=num_clusters)
+        labels_rgb = clustering_rgb.fit_predict(pixels_rgb)
         clustering_lab = KMeans(n_clusters=num_clusters)
         labels_lab = clustering_lab.fit_predict(pixels_lab)
+        show_comparison(image_cv_rgb, labels_rgb, image_cv_lab, labels_lab)
     elif segmentation_method.get() == "DBSCAN":
         eps = float(entry_eps.get())
         min_samples = int(entry_min_samples.get())
-        clustering = DBSCAN(eps=eps, min_samples=min_samples)
-        labels = clustering.fit_predict(pixels)
+        clustering_rgb = DBSCAN(eps=eps, min_samples=min_samples)
+        labels_rgb = clustering_rgb.fit_predict(pixels_rgb)
         clustering_lab = DBSCAN(eps=eps, min_samples=min_samples)
         labels_lab = clustering_lab.fit_predict(pixels_lab)
-    # elif segmentation_method.get() == "Region Growing Advanced":
-    #     if seeds:
-    #         output_image = grow_region_advanced(image_cv, seeds)
-    #         output_image = Image.fromarray(output_image)
-    #         img_tk = ImageTk.PhotoImage(output_image)
-    #         label_image.config(image=img_tk)
-    #         label_image.image = img_tk
-    elif segmentation_method.get() == "Seed Growing" and seeds:
-        output_image = grow_region(image_cv, seeds)
-        output_image = Image.fromarray(output_image)  # Преобразуем обратно в PIL Image для отображения в Tkinter
-        img_tk = ImageTk.PhotoImage(output_image)
+        show_comparison(image_cv_rgb, labels_rgb, image_cv_lab, labels_lab)
+    elif segmentation_method.get() == "Seed Growing":
+        output_image = grow_region(image_cv_rgb, initial_seed)
+        img_pil = Image.fromarray(output_image)
+        img_tk = ImageTk.PhotoImage(img_pil)
+        label_image.config(image=img_tk)
+        label_image.image = img_tk
+    elif segmentation_method.get() == "Region Growing":
+        output_image = grow_region_from_region(image_cv_rgb, initial_region)
+        img_pil = Image.fromarray(output_image)
+        img_tk = ImageTk.PhotoImage(img_pil)
         label_image.config(image=img_tk)
         label_image.image = img_tk
     elif segmentation_method.get() == "Watershed":
-        output_image = apply_watershed(image_cv)
-        output_image = (output_image * 255).astype(np.uint8)  # Убедимся, что данные в правильном формате
-        img_pil = Image.fromarray(output_image)  # Преобразуем массив в PIL Image
-        img_tk = ImageTk.PhotoImage(img_pil)  # Теперь создаем PhotoImage
+        output_image = apply_watershed(image_cv_rgb)
+        img_pil = Image.fromarray((output_image * 255).astype(np.uint8))
+        img_tk = ImageTk.PhotoImage(img_pil)
         label_image.config(image=img_tk)
         label_image.image = img_tk
 
-    # Generate colors for each label
-    unique_labels = np.unique(labels)
+
+def show_comparison(image_rgb, labels_rgb, image_lab, labels_lab):
+    unique_labels_rgb = np.unique(labels_rgb)
     unique_labels_lab = np.unique(labels_lab)
-    colors = [list(np.random.choice(range(256), size=3)) for _ in unique_labels]
+    colors_rgb = [list(np.random.choice(range(256), size=3)) for _ in unique_labels_rgb]
     colors_lab = [list(np.random.choice(range(256), size=3)) for _ in unique_labels_lab]
 
-    # Create segmented image arrays
-    segmented_img = np.zeros((image_cv.shape[0], image_cv.shape[1], 3), dtype=np.uint8)
-    segmented_img_lab = np.zeros((image_cv_lab.shape[0], image_cv_lab.shape[1], 3), dtype=np.uint8)
+    segmented_img_rgb = np.zeros((image_rgb.shape[0], image_rgb.shape[1], 3), dtype=np.uint8)
+    segmented_img_lab = np.zeros((image_lab.shape[0], image_lab.shape[1], 3), dtype=np.uint8)
 
-    # Map each pixel according to its label
-    for label, color in zip(unique_labels, colors):
-        segmented_img[labels.reshape(image_cv.shape[:2]) == label] = color
+    for label, color in zip(unique_labels_rgb, colors_rgb):
+        segmented_img_rgb[labels_rgb.reshape(image_rgb.shape[:2]) == label] = color
     for label, color in zip(unique_labels_lab, colors_lab):
-        segmented_img_lab[labels_lab.reshape(image_cv_lab.shape[:2]) == label] = color
+        segmented_img_lab[labels_lab.reshape(image_lab.shape[:2]) == label] = color
 
-    # Convert array to image for displaying
-    segmented_img = Image.fromarray(segmented_img)
-    segmented_img_lab = Image.fromarray(segmented_img_lab)
-    img_tk = ImageTk.PhotoImage(segmented_img)
-    label_image.config(image=img_tk)
-    label_image.image = img_tk
-
-    # Display comparison using Matplotlib
     plt.figure(figsize=(10, 5))
     plt.subplot(1, 2, 1)
-    plt.imshow(segmented_img)
+    plt.imshow(segmented_img_rgb)
     plt.title('RGB Space')
     plt.subplot(1, 2, 2)
     plt.imshow(segmented_img_lab)
@@ -107,68 +101,41 @@ def apply_segmentation():
     plt.show()
 
 
-# def grow_region_advanced(image, seeds, delta=30):
-#     # Преобразуем изображение в массив
-#     img_array = np.array(image)
-#     # Маска для отслеживания принадлежности к региону
-#     region_mask = np.zeros(img_array.shape[:2], dtype=int)
-#     region_index = 1
-#
-#     # Регионы и их статистика
-#     regions = {}
-#
-#     for seed in seeds:
-#         x, y = seed
-#         regions[region_index] = {
-#             'pixels': [(x, y)],
-#             'intensity': img_array[x, y],
-#             'average_intensity': img_array[x, y]
-#         }
-#         region_mask[x, y] = region_index
-#         region_index += 1
-#
-#     def update_region_stats(region_id, new_pixel):
-#         x, y = new_pixel
-#         region = regions[region_id]
-#         region['pixels'].append((x, y))
-#         new_avg = np.mean([img_array[p] for p in region['pixels']], axis=0)
-#         regions[region_id]['average_intensity'] = new_avg
-#
-#     # Проверка пикселей для добавления в регионы
-#     for region_id, data in regions.items():
-#         to_check = data['pixels'][:]
-#         while to_check:
-#             x, y = to_check.pop(0)
-#             for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-#                 nx, ny = x + dx, y + dy
-#                 if 0 <= nx < img_array.shape[0] and 0 <= ny < img_array.shape[1] and region_mask[nx, ny] == 0:
-#                     current_intensity = img_array[nx, ny]
-#                     intensity_diff = np.linalg.norm(current_intensity - data['average_intensity'])
-#
-#                     # Проверяем условия для включения пикселя в регион
-#                     if intensity_diff < delta:
-#                         region_mask[nx, ny] = region_id
-#                         update_region_stats(region_id, (nx, ny))
-#                         to_check.append((nx, ny))
-#
-#     # Создаем изображение результата
-#     output_image = np.zeros_like(img_array)
-#     for region_id, data in regions.items():
-#         for px in data['pixels']:
-#             output_image[px] = data['average_intensity']
-#
-#     return output_image
+def show_dbscan_variations():
+    image_cv = cv2.imread(path)
+    image_cv_rgb = cv2.cvtColor(image_cv, cv2.COLOR_BGR2RGB)
+    pixels = image_cv_rgb.reshape((-1, 3))
+
+    fig, axes = plt.subplots(nrows=1, ncols=4, figsize=(12, 8))
+    axes = axes.flatten()
+
+    parameters = [(1, 4), (2, 4), (3, 4), (4, 4)]
+    fixed_colors = generate_fixed_colors(256)
+
+    for ax, (eps, min_samples) in zip(axes, parameters):
+        clustering = DBSCAN(eps=eps, min_samples=min_samples)
+        labels = clustering.fit_predict(pixels)
+        unique_labels = np.unique(labels)
+
+        segmented_img = np.zeros((image_cv_rgb.shape[0], image_cv_rgb.shape[1], 3), dtype=np.uint8)
+        for label in unique_labels:
+            color = fixed_colors[label % len(fixed_colors)]
+            segmented_img[labels.reshape(image_cv_rgb.shape[:2]) == label] = color
+
+        ax.imshow(segmented_img)
+        ax.set_title(f"EPS: {eps}, Min Samples: {min_samples}")
+        ax.axis('off')
+
+    plt.tight_layout()
+    plt.show()
 
 
-def grow_region(image, seeds, threshold=200):
-    # Преобразуем изображение в массив
+def grow_region(image, initial_seed):
     img_array = np.array(image)
-    # Маска для отслеживания посещенных и добавленных пикселей
     mask = np.zeros(img_array.shape[:2], dtype=bool)
     output_image = np.zeros_like(img_array)
 
-    for seed in seeds:
-        x, y = seed
+    def region_grow(x, y):
         seed_value = img_array[x, y]
         to_grow = [(x, y)]
         while to_grow:
@@ -177,31 +144,81 @@ def grow_region(image, seeds, threshold=200):
                 continue
             mask[x, y] = True
             output_image[x, y] = seed_value
-            # Проверка соседей
             for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
                 nx, ny = x + dx, y + dy
                 if 0 <= nx < img_array.shape[0] and 0 <= ny < img_array.shape[1]:
                     if not mask[nx, ny]:
                         color_diff = np.linalg.norm(img_array[nx, ny] - seed_value)
-                        if color_diff < threshold:
+                        if color_diff < 220:
                             to_grow.append((nx, ny))
+
+    # Начать сегментацию из первого выбранного семени
+    x, y = initial_seed
+    if not mask[x, y]:
+        region_grow(x, y)
+
+    # Продолжить сегментацию из всех непосещенных пикселей
+    for x in range(img_array.shape[0]):
+        for y in range(img_array.shape[1]):
+            if not mask[x, y]:
+                region_grow(x, y)
+
     return output_image
 
 
-def apply_watershed(image):
-    # Преобразование в градации серого
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+def grow_region_from_region(image, initial_region):
+    img_array = np.array(image)
+    mask = np.zeros(img_array.shape[:2], dtype=bool)
+    output_image = np.zeros_like(img_array)
 
-    # Вычисление градиента изображения
+    start, end = initial_region
+
+    def region_grow(x, y):
+        seed_value = img_array[x, y]
+        to_grow = [(x, y)]
+        while to_grow:
+            x, y = to_grow.pop(0)
+            if mask[x, y]:
+                continue
+            mask[x, y] = True
+            output_image[x, y] = seed_value
+            for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                nx, ny = x + dx, y + dy
+                if 0 <= nx < img_array.shape[0] and 0 <= ny < img_array.shape[1]:
+                    if not mask[nx, ny]:
+                        color_diff = np.linalg.norm(img_array[nx, ny] - seed_value)
+                        if color_diff < 220:
+                            to_grow.append((nx, ny))
+
+    # Инициировать рост из всех пикселей в начальном регионе
+    for i in range(start[0], end[0]):
+        for j in range(start[1], end[1]):
+            if not mask[i, j]:
+                region_grow(i, j)
+
+    # Продолжить сегментацию из всех непосещенных пикселей
+    for x in range(img_array.shape[0]):
+        for y in range(img_array.shape[1]):
+            if not mask[x, y]:
+                region_grow(x, y)
+
+    return output_image
+
+
+def apply_gaussian_blur(image, kernel_size=5):
+    return cv2.GaussianBlur(image, (kernel_size, kernel_size), 0)
+
+def apply_watershed(image):
+    smoothed = apply_gaussian_blur(image)
+
+    gray = cv2.cvtColor(smoothed, cv2.COLOR_BGR2GRAY)
+
     gradient = filters.sobel(gray)
 
-    # Идентификация маркеров (минимумы градиента)
     markers = ndi.label(gradient < filters.threshold_otsu(gradient))[0]
 
-    # Применение метода водораздела
     labels = watershed(gradient, markers)
 
-    # Преобразование меток в цветное изображение для визуализации
     segmented_image = color.label2rgb(labels, image, kind='overlay')
 
     return segmented_image
@@ -232,13 +249,19 @@ def update_params(event):
 
 
 def on_image_click(event):
-    x, y = event.x, event.y
-    seed = (y, x)  # Обратите внимание на порядок, так как tkinter возвращает координаты в формате x, y
-    if 'seeds' not in globals():
-        global seeds
-        seeds = []
-    seeds.append(seed)
-    apply_segmentation()  # Обновляем изображение каждый раз после добавления семени
+    global initial_seed, rect_start, initial_region
+    if segmentation_method.get() == "Seed Growing":
+        x, y = event.x, event.y
+        initial_seed = (y, x)
+        apply_segmentation()
+    elif segmentation_method.get() == "Region Growing":
+        if 'rect_start' not in globals():
+            rect_start = (event.y, event.x)
+        else:
+            rect_end = (event.y, event.x)
+            initial_region = (rect_start, rect_end)
+            apply_segmentation()
+            del rect_start
 
 
 root = tk.Tk()
@@ -254,7 +277,8 @@ rb_color = tk.Radiobutton(root, text="Color Segmentation", variable=segment_type
 rb_color.pack()
 
 segmentation_method = StringVar(value="KMeans")
-segmentation_menu = tk.OptionMenu(root, segmentation_method, "KMeans", "DBSCAN", "Seed Growing", "Watershed",
+segmentation_menu = tk.OptionMenu(root, segmentation_method, "KMeans", "DBSCAN", "Seed Growing", "Region Growing",
+                                  "Watershed",
                                   command=update_params)
 segmentation_menu.pack()
 
@@ -277,9 +301,12 @@ option_menu.pack()
 btn_segment = tk.Button(root, text="Segment Image", command=apply_segmentation)
 btn_segment.pack()
 
+btn_show_variations = tk.Button(root, text="Show DBSCAN Variations", command=show_dbscan_variations)
+btn_show_variations.pack()
+
 label_image = tk.Label(root)
 label_image.pack()
 
-label_image.bind("<Button-1>", on_image_click)  # Привязка события клика к изображению
+label_image.bind("<Button-1>", on_image_click)
 
 root.mainloop()
